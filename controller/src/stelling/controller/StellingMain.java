9b67fe15692e827f30ff00a1b45471dd95fa88bf
Christian Hansen <sundgaarden@gmail.com>
2011-05-12 05:17:12 +0800

    added main class to controller package

---
+++ controller/src/stelling/controller/StellingMain.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class StellingMain {
+
+	/**
+	 * The main entry point for the application
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
